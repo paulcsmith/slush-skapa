8940ed954d4d773da97e706a8a2fcdde49ef201a
enatario <elaina.natario@gmail.com>
2018-04-23 14:43:18 -0400

    remove unnecessary js utils

--- templates/src/js/app.js
+++ templates/src/js/app.js
@@ -1,8 +1,6 @@
-import setPathCookie from './utils/setPathCookie.js'
 import removeMobileHover from './utils/removeMobileHover.js'
 
 removeMobileHover()
-setPathCookie()
 
 // Add class to html if JS is loaded
 document.querySelector('html').classList.add('js-is-loaded')
